6c9331affa1630ac750a474d5cb13508be6ab7fb
thanhdat <43725593+GiangThanhDat@users.noreply.github.com>
2020-08-03 22:53:39 +0700

    Update collectController.php

--- mvc/controllers/collectController.php
+++ mvc/controllers/collectController.php
@@ -54,160 +54,183 @@ class collectController extends controller
 			$data_string = "";
 			$fileName = "receive.txt";
 			date_default_timezone_set('Asia/Ho_Chi_Minh');
-			$time = date('Y-m-d H:i:s');
-			$dataModel = new data;
+			$time = date('Y-m-d H:i:s');			
+			$giatri = $this->model("giatri");
 			foreach ($data_keys as $key) {
 				$data_string =  $time ."=".$key."=".$data[$key]."\n";
 				$dataString = $this->dataProcess($data_string);
 				$dataObj = json_decode($dataString,true);
-				$dataModel->InsertObject("giatri",$dataObj);
+				$giatri->add($dataObj);				
 			}
 		}
 	}
+
+	// function load($ma_tram){
+	// 	$dataObj = new data;
+	// 	$getSensorMeasures = "SELECT DISTINCT `ma_cambien`, `ma_dailuong` FROM giatri WHERE  `ma_tram` = '$ma_tram'";
+	// 	$number_of_sensors = "SELECT DISTINCT `ma_cambien` FROM `giatri` WHERE `ma_tram` = '$ma_tram'";	
+	// 	$result = $dataObj->execute($getSensorMeasures);	
+	// 	$result2 = $dataObj->execute($number_of_sensors);
+		
+	// 	$cambienObject = $this->model("cambien");
+
+	// 	$listSensorMeasures = [];
+	// 	$listSensorOfStation = [];
+
+	// 	if($result2){
+	// 		while ($row = $result2->fetch_assoc()) {
+	// 			$sensorKey = $row['ma_cambien'];
+	// 			array_push($listSensorOfStation,json_decode($cambienObject->getByKey($sensorKey),true));
+	// 		}
+	// 	}
+
+	// 	if($result){
+	// 		while ($row = $result->fetch_assoc()) {				
+	// 			array_push($listSensorMeasures,$row);
+	// 		}	
+
+	// 		$nguoiquanlyObject = $this->model("nguoiquanly"); // mặc định vì phải có vì phải đang nhập mới sử dụng 
+	// 		$tramquantracObject = $this->model("tramquantrac");
+	// 		$donvido = $this->model("donvido");
+	// 		$nguoiquanlyObj = $nguoiquanlyObject->getByKey($_SESSION['taikhoan_nql'],$_SESSION['adm']);
+	// 		$tramquantracObj = $tramquantracObject->getByKey($ma_tram);
+	// 		$donvidoList = $donvido->listAll();
+	// 		//view			
+	// 		$model = "tramquantrac";
+	// 		$model_view = strtolower($model)."-view";		
+	// 		$pages		= "detail";
+
+	// 		$dataSend = [
+	// 			"model-view"=>$model_view, // cambien-view
+	// 			"model"		=>$model, // CamBien
+	// 			"pages"		=>$pages,					
+	// 			"nql_obj"   =>$nguoiquanlyObj,
+	// 			"tram_obj"  =>$tramquantracObj,
+	// 			"unitsList" =>$donvidoList,
+	// 			"sensorsStationList" =>json_encode($listSensorOfStation),
+ // 				"sensorMeasuresList"=>json_encode($listSensorMeasures)
+	// 		];
+			
+	// 		$this->view("masterLayout",$dataSend);
+	// 	}		
+	// }
 
 	function load($ma_tram){
-		$dataObj = new data;
-		$getSensorMeasures = "SELECT DISTINCT `ma_cambien`, `ma_dailuong` FROM giatri WHERE  `ma_tram` = '$ma_tram'";
-		$number_of_sensors = "SELECT DISTINCT `ma_cambien` FROM `giatri` WHERE `ma_tram` = '$ma_tram'";	
-		$result = $dataObj->execute($getSensorMeasures);	
-		$result2 = $dataObj->execute($number_of_sensors);
-		
+		//prepare model		
 		$cambienObject = $this->model("cambien");
+		$giatri = $this->model("giatri");
+		$nguoiquanlyObject = $this->model("nguoiquanly"); // mặc định vì phải có vì phải đang nhập mới sử dụng 
+		$tramquantracObject = $this->model("tramquantrac");
+		$donvido = $this->model("donvido");
 
-		$listSensorMeasures = [];
-		$listSensorOfStation = [];
+		//get data from database
+		$listSensorMeasures = $giatri->getSensorMeasureKeys($ma_tram);
+		$listSensorOfStation = $giatri->getNumbersOfSensor($ma_tram);
+		$nguoiquanlyObj = $nguoiquanlyObject->getByKey($_SESSION['taikhoan_nql'],$_SESSION['adm']);
+		$tramquantracObj = $tramquantracObject->getByKey($ma_tram);
+		$donvidoList = $donvido->listAll();
+		//view			
+		$model = "tramquantrac";
+		$model_view = strtolower($model)."-view";		
+		$pages		= "detail";
 
-		if($result2){
-			while ($row = $result2->fetch_assoc()) {
-				$sensorKey = $row['ma_cambien'];
-				array_push($listSensorOfStation,json_decode($cambienObject->getByKey($sensorKey),true));
-			}
-		}
+		$dataSend = [
+			"model-view"=>$model_view, // cambien-view
+			"model"		=>$model, // CamBien
+			"pages"		=>$pages,					
+			"nql_obj"   =>$nguoiquanlyObj,
+			"tram_obj"  =>$tramquantracObj,
+			"unitsList" =>$donvidoList,
+			"sensorsStationList" =>json_encode($listSensorOfStation),
+			"sensorMeasuresList"=>json_encode($listSensorMeasures)
+		];
+		
+		$this->view("masterLayout",$dataSend);
 
-		if($result){
-			while ($row = $result->fetch_assoc()) {				
-				array_push($listSensorMeasures,$row);
-			}	
-
-			$nguoiquanlyObject = $this->model("nguoiquanly"); // mặc định vì phải có vì phải đang nhập mới sử dụng 
-			$tramquantracObject = $this->model("tramquantrac");
-			$donvido = $this->model("donvido");
-			$nguoiquanlyObj = $nguoiquanlyObject->getByKey($_SESSION['taikhoan_nql'],$_SESSION['adm']);
-			$tramquantracObj = $tramquantracObject->getByKey($ma_tram);
-			$donvidoList = $donvido->listAll();
-			//view			
-			$model = "tramquantrac";
-			$model_view = strtolower($model)."-view";		
-			$pages		= "detail";
-
-			$dataSend = [
-				"model-view"=>$model_view, // cambien-view
-				"model"		=>$model, // CamBien
-				"pages"		=>$pages,					
-				"nql_obj"   =>$nguoiquanlyObj,
-				"tram_obj"  =>$tramquantracObj,
-				"unitsList" =>$donvidoList,
-				"sensorsStationList" =>json_encode($listSensorOfStation),
- 				"sensorMeasuresList"=>json_encode($listSensorMeasures)
-			];
-			
-			$this->view("masterLayout",$dataSend);
-		}		
 	}
 
+
+	// function get($ma_tram,$ma_cambien,$ma_dailuong)
+	// {
+	// 	$getLatestVal = "SELECT `giatri`,`thoigian` FROM `giatri` WHERE `ma_tram` = '$ma_tram' AND `ma_cambien` = '$ma_cambien' AND `ma_dailuong` = '$ma_dailuong' ORDER BY thoigian DESC LIMIT 1";
+	// 	$dataObj = new data;
+	// 	$result = $dataObj->execute($getLatestVal);		
+	// 	if($result->num_rows==1){
+	// 		$val = $result->fetch_assoc();
+	// 		$myVal = [
+	// 			"val"=>$val['giatri'],
+	// 			"time"=>$val['thoigian']
+	// 		];
+	// 		echo json_encode($myVal);
+	// 	}else{
+	// 		echo 'null';
+	// 	}
+
+	// 	//echo $this->model("giatri")->get($ma_tram,$ma_cambien,$ma_dailuong);
+	// }
 
 	function get($ma_tram,$ma_cambien,$ma_dailuong)
 	{
-		$getLatestVal = "SELECT `giatri`,`thoigian` FROM `giatri` WHERE `ma_tram` = '$ma_tram' AND `ma_cambien` = '$ma_cambien' AND `ma_dailuong` = '$ma_dailuong' ORDER BY thoigian DESC LIMIT 1";
-		$dataObj = new data;
-		$result = $dataObj->execute($getLatestVal);		
-		if($result->num_rows==1){
-			$val = $result->fetch_assoc();
-			$myVal = [
-				"val"=>$val['giatri'],
-				"time"=>$val['thoigian']
-			];
-			echo json_encode($myVal);
-		}else{
-			echo 'null';
-		}
+		echo $this->model("giatri")->get($ma_tram,$ma_cambien,$ma_dailuong);
 	}
+
 
 
 	function addDL($ma_tram,$ma_cambien)
-	{	
-
-		$number_of_measures = "SELECT DISTINCT `ma_dailuong` FROM giatri WHERE `ma_tram` = '$ma_tram' AND `ma_cambien` = '$ma_cambien'";
-
-		$dailuongdoObject = $this->model("dailuongdo");
+	{		
+		$giatri = $this->model("giatri");
 		$cambienObject = $this->model("cambien");
-
-		$dataObj = new data;
-		$result = $dataObj->execute($number_of_measures);
-		$list = [];
-
-		if($result){
-			while ($row = $result->fetch_assoc()) {			
-				$measureKey = $row['ma_dailuong'];
-				array_push($list,json_decode($dailuongdoObject->getByKey($measureKey),true));
-			}
-
-			$model = "dailuongdo";
-			$model_view = strtolower($model)."-view";		
-			$pages		= "add-form";
-			$nguoiquanlyObject = $this->model("nguoiquanly"); // mặc định vì phải có vì phải đang nhập mới sử dụng 
-			$tramquantracObject = $this->model("tramquantrac");
-			$tramquantracObj = $tramquantracObject->getByKey($ma_tram);
-			$nguoiquanlyObj = $nguoiquanlyObject->getByKey($_SESSION['taikhoan_nql'],$_SESSION['adm']);
-			$cambienObj = $cambienObject->getByKey($ma_cambien);
-
-			$dataSend = [
-				"model-view"=>$model_view, // cambien-view
-				"model"		=>$model, // CamBien
-				"pages"		=>$pages,					
-				"nql_obj"   =>$nguoiquanlyObj,
-				"cb_obj"	=>$cambienObj,
-				"tram_obj"  =>$tramquantracObj,
-				"listMeasuresBySensor"=>json_encode($list)
-			];
+		$tramquantracObject = $this->model("tramquantrac");
+		$nguoiquanlyObject = $this->model("nguoiquanly"); // mặc định vì phải có vì phải đang nhập mới sử dụng 
 			
-			$this->view("masterLayout",$dataSend);
-		}
+		//data
+		$cambienObj = $cambienObject->getByKey($ma_cambien);
+		$tramquantracObj = $tramquantracObject->getByKey($ma_tram);
+		$nguoiquanlyObj = $nguoiquanlyObject->getByKey($_SESSION['taikhoan_nql'],$_SESSION['adm']);
+		$list = $giatri->getListMeasuresBySensor($ma_tram,$ma_cambien);
+
+		$model = "dailuongdo";
+		$model_view = strtolower($model)."-view";		
+		$pages		= "add-form";
+		$dataSend = [
+			"model-view"=>$model_view, // cambien-view
+			"model"		=>$model, // CamBien
+			"pages"		=>$pages,					
+			"nql_obj"   =>$nguoiquanlyObj,
+			"cb_obj"	=>$cambienObj,
+			"tram_obj"  =>$tramquantracObj,
+			"listMeasuresBySensor"=>json_encode($list),
+			"myList"=>json_encode($list)
+		];
+		
+		$this->view("masterLayout",$dataSend);
+	
 	}
 
 	function addCB($ma_tram){
-		$number_of_sensors = "SELECT DISTINCT `ma_cambien` FROM `giatri` WHERE `ma_tram` = '$ma_tram'";	
-		$cambienObject = $this->model("cambien");
-		$dataObj = new data;
-		$result = $dataObj->execute($number_of_sensors);
-		$list = [];
+		//preare model
+		$giatri = $this->model("giatri");
+		$nguoiquanlyObject = $this->model("nguoiquanly"); // mặc định vì phải có vì phải đang nhập mới sử dụng 
+		$tramquantracObject = $this->model("tramquantrac");
 
-		if($result){
-			while ($row = $result->fetch_assoc()) {
-				$sensorKey = $row['ma_cambien'];
-				array_push($list,json_decode($cambienObject->getByKey($sensorKey),true));
-			}
-			$model = "cambien";
-			$model_view = strtolower($model)."-view";		
-			$pages		= "add-form";
-			$nguoiquanlyObject = $this->model("nguoiquanly"); // mặc định vì phải có vì phải đang nhập mới sử dụng 
-			$tramquantracObject = $this->model("tramquantrac");
+		//data		
+		$nguoiquanlyObj = $nguoiquanlyObject->getByKey($_SESSION['taikhoan_nql'],$_SESSION['adm']);
+		$tramquantracObj = $tramquantracObject->getByKey($ma_tram);			
+		$list = $giatri->getListSensorsByStation($ma_tram);
 
-
-			$nguoiquanlyObj = $nguoiquanlyObject->getByKey($_SESSION['taikhoan_nql'],$_SESSION['adm']);
-			$tramquantracObj = $tramquantracObject->getByKey($ma_tram);			
-
-			$dataSend = [
-				"model-view"=>$model_view, // cambien-view
-				"model"		=>$model, // CamBien
-				"pages"		=>$pages,					
-				"nql_obj"   =>$nguoiquanlyObj,				
-				"tram_obj"  =>$tramquantracObj,
-				"listSensorsByStation"=>json_encode($list)
-			];
-			
-			$this->view("masterLayout",$dataSend);
-		}		
+		$model = "cambien";
+		$model_view = strtolower($model)."-view";		
+		$pages		= "add-form";
+		$dataSend = [
+			"model-view"=>$model_view, // cambien-view
+			"model"		=>$model, // CamBien
+			"pages"		=>$pages,					
+			"nql_obj"   =>$nguoiquanlyObj,				
+			"tram_obj"  =>$tramquantracObj,
+			"listSensorsByStation"=>json_encode($list),
+			"myList"=>json_encode($list)
+		];		
+		$this->view("masterLayout",$dataSend);		
 	}
 
 
@@ -226,24 +249,53 @@ class collectController extends controller
 		}
 	}
 
+	// function chart($ma_tram,$ma_cambien,$ma_dailuong)
+	// {
+	// 	$dataObj = new data;
+	// 	$getSensorMeasures = "SELECT DISTINCT `ma_cambien`, `ma_dailuong` FROM giatri WHERE  `ma_tram` = '$ma_tram'";
+	// 	$result = $dataObj->execute($getSensorMeasures);
+	// 	$listSensorMeasures = [];
+	// 	if($result){
+	// 		while ($row = $result->fetch_assoc()) {				
+	// 			array_push($listSensorMeasures,$row);
+	// 		}
+	// 	}
+	// 	// thông tin mặc định cho một trang show
+	// 	$model = "tramquantrac";
+	// 	$model_view = strtolower($model)."-view";
+	// 	$pages		= "chart-type";
+	// 	$nguoiquanlyObject = $this->model("nguoiquanly");
+	// 	$nguoiquanlyObj = $nguoiquanlyObject->getByKey($_SESSION['taikhoan_nql'],$_SESSION['adm']);
+	// 	$keys = ["ma_tram"=>$ma_tram,"ma_cambien"=>$ma_cambien,"ma_dailuong"=>$ma_dailuong];
+	// 	$data = [
+	// 		"model-view" =>$model_view, //cambien-view
+	// 		"model"		 =>$model, //CamBien
+	// 		"pages"		 =>$pages,
+	// 		"keys"		 =>json_encode($keys),
+	// 		"nql_obj"	 =>$nguoiquanlyObj,
+ // 			"sensorMeasuresList"=>json_encode($listSensorMeasures)			
+	// 	];
+	// 	$this->view("masterLayout",$data);
+	// }
+
+
+
 	function chart($ma_tram,$ma_cambien,$ma_dailuong)
 	{
-		$dataObj = new data;
-		$getSensorMeasures = "SELECT DISTINCT `ma_cambien`, `ma_dailuong` FROM giatri WHERE  `ma_tram` = '$ma_tram'";
-		$result = $dataObj->execute($getSensorMeasures);
-		$listSensorMeasures = [];
-		if($result){
-			while ($row = $result->fetch_assoc()) {				
-				array_push($listSensorMeasures,$row);
-			}
-		}
+		//prepare model
+		$giatri = $this->model("giatri");
+		$nguoiquanlyObject = $this->model("nguoiquanly");
+		$nguoiquanlyObj = $nguoiquanlyObject->getByKey($_SESSION['taikhoan_nql'],$_SESSION['adm']);
+
+		//data
+		$listSensorMeasures = $giatri->getSensorMeasureKeys($ma_tram);
+
 		// thông tin mặc định cho một trang show
 		$model = "tramquantrac";
 		$model_view = strtolower($model)."-view";
-		$pages		= "chart-type";
-		$nguoiquanlyObject = $this->model("nguoiquanly");
-		$nguoiquanlyObj = $nguoiquanlyObject->getByKey($_SESSION['taikhoan_nql'],$_SESSION['adm']);
+		$pages		= "chart-type";		
 		$keys = ["ma_tram"=>$ma_tram,"ma_cambien"=>$ma_cambien,"ma_dailuong"=>$ma_dailuong];
+
 		$data = [
 			"model-view" =>$model_view, //cambien-view
 			"model"		 =>$model, //CamBien
